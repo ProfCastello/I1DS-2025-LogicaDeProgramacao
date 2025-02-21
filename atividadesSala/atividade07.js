8ae7a01570574d6e040287a60e6e6fe3ae6e4d31
ProfCastello <gustavo.castello@sp.senai.br>
2025-02-21 08:42:28 -0300

    desenvolvendo atividade 7

---
+++ atividadesSala/atividade07.js
@@ -0,0 +1,11 @@
+/***************************************************** 
+             Lista de Exercícios 07
+*****************************************************/ 
+/* 
+ Dada a matriz abaixo, faça um programa em JavaScript 
+ que exiba ao usuário os elementos da sua diagonal principal com o laço FOR. 
+  
+ |   3   5   8   | 
+ |   1   9   2   | 
+ |   7   1   4   | 
+*/ 
